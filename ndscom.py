cc7f494396d0c28c0dbfb8e3a359a2590d30a1da
jovistar <jovistar@gmail.com>
2014-03-31 17:08:14 +0800

    Update Ndscom

--- ndscom.py
+++ ndscom.py
@@ -2,18 +2,53 @@
 
 import socket
 import json
+import os
 
-def test():
-	address = ('127.0.0.1', 12321)
+class NdsCom():
+	def __init__(self, host, port):
+		self.address = (host, port)
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	def create(self, path):
+		data = {}
+		data['request'] = 'create'
+		
+		if not os.path.isfile(path):
+			return 1, ''
+		data['path'] = os.path.abspath(path)
 
-	data = {'request':'create', 'path':'/root/nDroid-Storage/s.apk'}
-	msg = json.dumps(data)
+		result = self.doCom(data)
+		if result['response'] != 0:
+			return 1, ''
+		else:
+			return 0, result['uid']
 
-	s.sendto(msg, address)
-	result,addr = s.recvfrom(2048)
-	print result
 
-if __name__ == '__main__':
-	test()
+	def delete(self, uid):
+		data = {}
+		data['request'] = 'delete'
+		data['uid'] = uid
+
+		result = self.doCom(data)
+		if result['response'] != 0:
+			return 1
+		else:
+			return 0
+
+	def get(self, uid):
+		data = {}
+		data['request'] = 'get'
+		data['uid'] = uid
+
+		result = self.doCom(data)
+		if result['response'] != 0:
+			return 1, ''
+		else:
+			return 0, result['path']
+
+	def doCom(self, data):
+		msg = json.dumps(data)
+		self.s.sendto(msg, self.address)
+		result,addr = self.s.recvfrom(4096)
+
+		return json.loads(result)
